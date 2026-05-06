df859317916f3d9ef078fd3d18e6f12767257ee2
Aduy1402 <nguyenanhduy950@gmail.com>
2026-05-06 18:20:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Client, GatewayIntentBits } = require('discord.js');
 const cron = require('node-cron');
 
@@ -6,9 +5,9 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-let daysLeft = 324;
+let daysLeft = 365;
 
-const CHANNEL_ID = '1501534980227072141';
+const CHANNEL_ID = 'DAN_ID_VAO_DAY';
 
 client.once('ready', () => {
   console.log('Bot đã online');
@@ -30,37 +29,4 @@ client.once('ready', () => {
   });
 });
 
-=======
-const { Client, GatewayIntentBits } = require('discord.js');
-const cron = require('node-cron');
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-let daysLeft = 324;
-
-const CHANNEL_ID = '1501534980227072141';
-
-client.once('ready', () => {
-  console.log('Bot đã online');
-
-  cron.schedule('0 0 * * *', async () => {
-    daysLeft--;
-
-    const channel = await client.channels.fetch(CHANNEL_ID);
-
-    if (channel) {
-      if (daysLeft > 0) {
-        channel.send(`📅 Còn ${daysLeft} ngày!`);
-      } else {
-        channel.send(`🎉 XONG RỒI!!!`);
-      }
-    }
-  }, {
-    timezone: "Asia/Ho_Chi_Minh"
-  });
-});
-
->>>>>>> dcf7bc5c54f0f0447f584d8268a8e0a20d0ecc08
 client.login(process.env.TOKEN);
